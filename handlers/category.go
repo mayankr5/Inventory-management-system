11acd27748892eb2c112399c257996e404d1fc25
Mayank <mayank525.work@gmail.com>
2023-12-14 14:33:14 +0530

    Add Categories API's

---
+++ handlers/category.go
@@ -0,0 +1,125 @@
+package handler
+
+import (
+	"database/sql"
+	"strconv"
+
+	"github.com/inventory-management-system/models"
+	"gofr.dev/pkg/errors"
+	"gofr.dev/pkg/gofr"
+)
+
+func GetCategories(ctx *gofr.Context) (interface{}, error) {
+
+	rows, err := ctx.DB().QueryContext(ctx, " SELECT category_id, name FROM categories")
+	if err != nil {
+		return nil, errors.DB{Err: err}
+	}
+
+	defer rows.Close()
+
+	categories := make([]models.Category, 0)
+
+	for rows.Next() {
+		var category models.Category
+
+		err = rows.Scan(&category.ID, &category.Name)
+		if err != nil {
+			return nil, errors.DB{Err: err}
+		}
+
+		categories = append(categories, category)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		return nil, errors.DB{Err: err}
+	}
+
+	return categories, nil
+}
+
+func GetCategoryByID(ctx *gofr.Context) (interface{}, error) {
+	id := ctx.PathParam("id")
+
+	if id == "" {
+		return nil, errors.MissingParam{Param: []string{"id"}}
+	}
+
+	if _, err := strconv.Atoi(id); err != nil {
+		return nil, errors.InvalidParam{Param: []string{"id"}}
+	}
+
+	var category models.Category
+
+	err := ctx.DB().QueryRowContext(ctx, " SELECT category_id, name FROM categories WHERE category_id=$1", id).
+		Scan(&category.ID, &category.Name)
+
+	if err == sql.ErrNoRows {
+		return nil, errors.EntityNotFound{Entity: "product", ID: id}
+	}
+
+	return category, nil
+}
+
+func AddCategory(ctx *gofr.Context) (interface{}, error) {
+	var category models.Category
+
+	if err := ctx.Bind(&category); err != nil {
+		ctx.Logger.Errorf("error in binding: %v", err)
+		return nil, errors.InvalidParam{Param: []string{"body"}}
+	}
+
+	_, err := ctx.DB().ExecContext(ctx, "INSERT INTO categories(name) VALUES($1)", category.Name)
+	if err != nil {
+		return nil, err
+	}
+
+	return "Successfully added", nil
+}
+
+func UpdateCategory(ctx *gofr.Context) (interface{}, error) {
+	id := ctx.PathParam("id")
+
+	if id == "" {
+		return nil, errors.MissingParam{Param: []string{"id"}}
+	}
+
+	if _, err := strconv.Atoi(id); err != nil {
+		return nil, errors.InvalidParam{Param: []string{"id"}}
+	}
+
+	var category models.Category
+
+	if err := ctx.Bind(&category); err != nil {
+		ctx.Logger.Errorf("error in binding: %v", err)
+		return nil, errors.InvalidParam{Param: []string{"body"}}
+	}
+
+	_, err := ctx.DB().ExecContext(ctx, " UPDATE categories SET name = $1 WHERE category_id=$2 ", category.Name, id)
+	if err != nil {
+		return nil, errors.DB{Err: err}
+	}
+
+	return "Update successfully", nil
+}
+
+func DeleteCategory(ctx *gofr.Context) (interface{}, error) {
+	id := ctx.PathParam("id")
+
+	if id == "" {
+		return nil, errors.MissingParam{Param: []string{"id"}}
+	}
+
+	if _, err := strconv.Atoi(id); err != nil {
+		return nil, errors.InvalidParam{Param: []string{"id"}}
+	}
+
+	_, err := ctx.DB().ExecContext(ctx, "DELETE FROM categories WHERE category_id=$1", id)
+	if err != nil {
+		return nil, errors.DB{Err: err}
+	}
+
+	return "Deleted successfully", nil
+
+}
--- main.go
+++ main.go
@@ -8,13 +8,19 @@ import (
 func main() {
 	app := gofr.New()
 
+	//Product API's
 	app.GET("/products", handler.GetProducts)
 	app.GET("/products/{id}", handler.GetProductByID)
 	app.POST("/products", handler.AddProduct)
 	app.PUT("/products/{id}", handler.UpdateProduct)
 	app.DELETE("/products/{id}", handler.DeleteProduct)
 
-	// Starts the server, it will listen on the default port 8000.
-	// it can be over-ridden through configs
+	// Categories API's
+	app.GET("/categories", handler.GetCategories)
+	app.GET("/categories/{id}", handler.GetCategoryByID)
+	app.POST("/categories", handler.AddCategory)
+	app.PUT("/categories/{id}", handler.UpdateCategory)
+	app.DELETE("/categories/{id}", handler.DeleteCategory)
+
 	app.Start()
 }
